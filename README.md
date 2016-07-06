ea9ae4d692df8c8bde817d0894e8b4c7ad3b83ed
Mikael Andersson <mikael.andersson@volumental.com>
2016-07-06 23:51:11 +0200

    adding posting capabilities

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gadget-ekensberg-swim-thermo
-Everything for building and getting the gadget Ekensberg Swim Thermometer running
+Everything for building and getting the gadget Ekensberg Swim Thermometer running.
+
+## Run
+1. Copy script to pi
+1. Install deps on pi
+1. Run it in e.g. byobu
+
+## Dependencies
+requests, pyserial, py2.7
+
+## Todo 
+1. Tests
+1. Dockerize raspi
+1. Dockerize script
--- save_serial_data.py
+++ save_serial_data.py
@@ -1,6 +1,65 @@
+import requests
+import os
 import serial
 from time import strftime
 from datetime import datetime, time
+
+
+class DataPoster:
+
+	def __init__(self, token, base_url):
+		gadget_id = "1" 
+		self.post_url = base_url+'/api/v1/gadgets/'+gadget_id+'/data/'
+		self.headers = {'Authorization': 'Bearer '+token}
+
+	def post_data(data):
+		try:
+			payload = {
+				'data':data,
+				'timestamp': datetime.now().isoformat()
+			}
+			r = requests.post(self.post_url, json=payload, headers=self.headers)
+			print("Posted")
+		except:
+			print("Error when posting")
+
+	def data_from_string(data_string):
+		transmission_id = 57279
+		temp_deep = 17.50
+		temp_surface = 20.00
+		battery_v = 4.04
+		data = {
+			'transmission_id': transmission_id, 
+			'temp_deep"': temp_deep,
+			'temp_surface': temp_surface,
+			'battery_v': battery_v
+		}
+		return data
+
+	def post_data_from_string(data_string):
+		post_data(data_from_string(data_string))
+
+username = os.environ.get('GADGET_DATA_POSTER_USERNAME', None)
+password = os.environ.get('GADGET_DATA_POSTER_PASSWORD', None)
+base_url = os.environ.get('GADGET_DATA_POSTER_URL', '')
+
+try:
+	login_url = base_url + '/api-token-auth/'
+	credentials = {'username':username, 'password':password}
+	r = requests.post(login_url, json=credentials)
+	token=r.json()['token']
+	print('Token received')
+except:
+	print('No credentials...')
+	exit()
+
+data_poster = DataPoster(token, base_url)
+
+data_string = "57279,17.50,20.00,4.04"
+post_data_from_string(data)
+
+'''
+Main thread
 
 try:
 	ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # linux address
@@ -16,8 +75,9 @@ try:
 		if len(line)>0: 
 			now = datetime.now()
 			print("%s %s"%(now.strftime("%Y-%m-%d %H:%M:%S"),line))
-			f=open('temp_sensor_log.log','a')
+			f=open('temp_sensor.log','a')
 			print >> f, ("%s %s"%(now.strftime("%Y-%m-%d %H:%M:%S"),line))
 			f.close()
 except KeyboardInterrupt:
 	print("\ndone")
+'''
