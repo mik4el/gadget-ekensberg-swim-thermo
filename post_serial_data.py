c07345f4db186f9f6347dae715d660e0ac2b1b69
Mikael Andersson <mikael.andersson@volumental.com>
2016-08-08 20:54:48 +0200

    renewing tokens and posting status updates

--- post_serial_data.py
+++ post_serial_data.py
@@ -4,6 +4,7 @@ from time import strftime
 import requests
 from datetime import datetime, timedelta
 import threading
+import queue
 
 
 class DataAuther:
@@ -15,23 +16,21 @@ class DataAuther:
 		self.token = None
 
 	def auth_and_get_token(self):
-		try:
-			credentials = {'username':self.username, 'password':self.password}
-			r = requests.post(self.login_url, json=credentials)
-			self.token=r.json()['token']
-		except:
-			self.token=None
+		credentials = {'username':self.username, 'password':self.password}
+		r = requests.post(self.login_url, json=credentials)
+		self.token=r.json()['token']
 		return self.token
 
 
 class DataPosterWorker(threading.Thread):
 
-	def __init__(self, token, base_url, data_string):
+	def __init__(self, token, base_url, data_string, status_queue):
 		threading.Thread.__init__(self)
 		gadget_slug = "ekensberg-swim-thermo" 
 		self.post_url = base_url+'/api/v1/gadgets/'+gadget_slug+'/data/'
 		self.headers = {'Authorization': 'Bearer '+ token}
 		self.data_string = data_string
+		self.status_queue = status_queue
 
 	def run(self):
 		if self.data_string != "Setup complete!":
@@ -39,14 +38,24 @@ class DataPosterWorker(threading.Thread):
 
 	def post_data(self, data):
 		now = datetime.now() - timedelta(hours=2)  # convert to Z timezone
+		payload = {
+			'data':data,
+			'timestamp': now.isoformat()
+		}
 		try:
-			payload = {
-				'data':data,
-				'timestamp': now.isoformat()
-			}
 			r = requests.post(self.post_url, json=payload, headers=self.headers)
 		except:
-			raise Exception("Error when posting")
+			self.update_status("Error when posting.")
+			return
+		if r.status_code != 201:
+			self.update_status(r.status_code)
+			self.update_status(r.json())
+			self.update_status("Request not ok when posting.")
+		else:
+			self.update_status("Posted: '{}'".format(data))
+
+	def update_status(self, message):
+		self.status_queue.put(message)
 
 	def to_number(self, s):
 		try:
@@ -77,36 +86,52 @@ class DataPosterWorker(threading.Thread):
 	def post_data_from_string(self, data_string):
 		self.post_data(self.data_from_string(data_string))
 
+
 if __name__ == '__main__':
 	username = os.environ.get('GADGET_DATA_POSTER_USERNAME', None)
 	password = os.environ.get('GADGET_DATA_POSTER_PASSWORD', None)
 	base_url = os.environ.get('GADGET_DATA_POSTER_URL', '')
 
+	status_queue = queue.Queue()
 	data_auther = DataAuther(username, password, base_url)
 	token = data_auther.auth_and_get_token()
-
-	if token is None:
-		print("No token")
-		exit()
-
+	
 	try:
 		ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # linux address
 	except:
 		try:
 			ser = serial.Serial('/dev/cu.wchusbserialfd130', 9600, timeout=1)  # osx address 2
 		except:
-			ser = serial.Serial('/dev/cu.wchusbserial1410', 9600, timeout=1)  # osx address 1 
+			try:
+				ser = serial.Serial('/dev/cu.wchusbserial1410', 9600, timeout=1)  # osx address 1 
+			except:
+				print("No more serial ports to try. Exiting.")
+				exit()
 
 	try:
-		while 1:
+		while True:
+			while not status_queue.empty():
+				try:
+					message = status_queue.get()
+				except queue.Empty:
+					pass
+				else:
+					print("{:%Y-%m-%d %H:%M} {}" .format(datetime.now(),message))
+					if message == "Request not ok when posting.":
+						# reauth
+						token = data_auther.auth_and_get_token()
+						print("Token updated.")
+						with status_queue.mutex:
+							status_queue.queue.clear()
+
 			line=ser.readline().rstrip()
 			if len(line)>0:
 				line = line.decode('ascii')
-				data_poster = DataPosterWorker(token, base_url, line)
+				data_poster = DataPosterWorker(token, base_url, line, status_queue)
 				data_poster.start()
 				log_message = "{:%Y-%m-%d %H:%M} {}" .format(datetime.now(),line)
 				with open('temp_sensor.log','a') as logfile:
-					logfile.write(log_message)
+					print(log_message, file=logfile)
 				print(log_message)
 	except KeyboardInterrupt:
 		print("\ndone")
